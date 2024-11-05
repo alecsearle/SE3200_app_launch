9dee58d96b68313de097599a9b85ed93b08641c5
alecsearle <searle.alec@gmail.com>
2024-11-05 14:21:10 -0700

    app files

---
+++ client/app.js
@@ -0,0 +1,195 @@
+console.log("connected");
+
+let reviewsWrapper = document.querySelector("section");
+
+function addHikeReview(data) {
+  let newCard = document.createElement("div");
+  newCard.classList.add("card");
+
+  let hikeName = document.createElement("h2");
+  hikeName.textContent = data.name;
+
+  let hikeLocation = document.createElement("h3");
+  hikeLocation.textContent = data.location;
+
+  let hikeMiles = document.createElement("p");
+  hikeMiles.textContent = `${data.miles} miles`;
+
+  let starsContainer = document.createElement("div");
+  for (let i = 0; i < parseInt(data.rating); i++) {
+    let hikeRating = document.createElement("ion-icon");
+    hikeRating.setAttribute("name", "star");
+    starsContainer.appendChild(hikeRating);
+  }
+
+  let hikeReview = document.createElement("p");
+  hikeReview.textContent = data.review;
+
+  let hikePicture = document.createElement("img");
+  hikePicture.src = data.picture;
+  hikePicture.alt = "Picture of " + data.name;
+  hikePicture.classList.add("thumbnail");
+
+  let editButton = document.createElement("button");
+  editButton.textContent = "Edit";
+
+  let deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+
+  reviewsWrapper.appendChild(newCard);
+  newCard.appendChild(hikePicture);
+  newCard.appendChild(hikeName);
+  newCard.appendChild(hikeLocation);
+  newCard.appendChild(hikeMiles);
+  newCard.appendChild(starsContainer);
+  newCard.appendChild(hikeReview);
+  newCard.appendChild(editButton);
+  newCard.appendChild(deleteButton);
+
+  // Edit functionality
+  editButton.onclick = function () {
+    console.log("hike id:", data.id);
+
+    let editHikeName = document.querySelector("#edit-hike-name");
+    let editHikeLocation = document.querySelector("#edit-hike-location");
+    let editHikeMiles = document.querySelector("#edit-hike-miles");
+    let editHikeRating = document.querySelector("#edit-hike-rating");
+    let editHikeReview = document.querySelector("#edit-hike-review");
+    let editHikePicture = document.querySelector("#edit-hike-picture");
+
+    console.log(editHikeName.value);
+    console.log(editHikeLocation.value);
+    console.log(editHikeMiles.value);
+    console.log(editHikeRating.value);
+    console.log(editHikeReview.value);
+    console.log(editHikePicture.value);
+    //prep data to sent to server
+    let editData = "name=" + encodeURIComponent(editHikeName.value);
+    editData += "&location=" + encodeURIComponent(editHikeLocation.value);
+    editData += "&miles=" + encodeURIComponent(editHikeMiles.value);
+    editData += "&rating=" + encodeURIComponent(editHikeRating.value);
+    editData += "&review=" + encodeURIComponent(editHikeReview.value);
+    editData += "&picture=" + encodeURIComponent(editHikePicture.value);
+
+    fetch("http://localhost:8080/hikes/" + data.id, {
+      method: "PUT",
+      body: editData,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }).then(function (response) {
+      console.log("Updated Review", response);
+      reviewsWrapper.textContent = "";
+      loadReviewsFromServer();
+
+      editHikeName.value = "";
+      editHikeLocation.value = "";
+      editHikeMiles.value = "";
+      editHikeRating.value = "";
+      editHikeReview.value = "";
+      editHikePicture.value = "";
+    });
+  };
+
+  // Delete functionality
+  deleteButton.onclick = function () {
+    if (confirm("Do you actually wanna delete this?") == true) {
+      fetch("http://localhost:8080/hikes/" + data.id, {
+        method: "DELETE",
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log("Deleted hike with ID:", data.id);
+            newCard.remove(); // Remove the card element from the DOM
+          } else {
+            console.error("Failed to delete the hike review.");
+          }
+        })
+        .catch((error) => console.error("Error:", error));
+    }
+  };
+}
+
+function loadReviewsFromServer() {
+  fetch("http://localhost:8080/hikes")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      reviewsWrapper.innerHTML = ""; // Clear the reviewsWrapper
+      data.forEach((review) => {
+        addHikeReview(review); // Create a new review card for each review
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching reviews:", error);
+    });
+}
+
+// Call to load reviews when the page loads
+loadReviewsFromServer();
+
+// Add new review functionality
+let addReviewButton = document.querySelector("#add-review-button");
+
+function addNewReview() {
+  console.log("Add Review button clicked");
+  let inputHikeName = document.querySelector("#input-hike-name");
+  let inputHikeLocation = document.querySelector("#input-hike-location");
+  let inputHikeMiles = document.querySelector("#input-hike-miles");
+  let inputHikeRating = document.querySelector("#input-hike-rating");
+  let inputHikeReview = document.querySelector("#input-hike-review");
+  let inputHikePicture = document.querySelector("#input-hike-picture");
+
+  let data =
+    "name=" +
+    encodeURIComponent(inputHikeName.value) +
+    "&location=" +
+    encodeURIComponent(inputHikeLocation.value) +
+    "&miles=" +
+    encodeURIComponent(inputHikeMiles.value) +
+    "&rating=" +
+    encodeURIComponent(inputHikeRating.value) +
+    "&review=" +
+    encodeURIComponent(inputHikeReview.value) +
+    "&picture=" +
+    encodeURIComponent(inputHikePicture.value);
+
+  fetch("http://localhost:8080/hikes", {
+    method: "POST",
+    body: data,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  })
+    .then(function (response) {
+      if (response.ok) {
+        console.log("New review created!");
+        loadReviewsFromServer(); // Reload reviews after creating a new one
+        // Clear input fields
+        inputHikeName.value = "";
+        inputHikeLocation.value = "";
+        inputHikeMiles.value = "";
+        inputHikeRating.value = "";
+        inputHikeReview.value = "";
+        inputHikePicture.value = "";
+      } else {
+        console.error("Failed to create new review.");
+      }
+    })
+    .catch((error) => console.error("Error creating review:", error));
+}
+
+addReviewButton.onclick = addNewReview;
+
+// Toggle visibility for the add review container
+let addReviewBtn = document.getElementById("myBtn");
+let addReviewContainer = document.getElementById("addReviewContainer");
+
+addReviewBtn.onclick = function () {
+  addReviewContainer.classList.toggle("hidden");
+};
---
+++ server/server.py
@@ -0,0 +1,77 @@
+from flask import Flask, request
+from hikes import HikeDB
+
+app = Flask(__name__)
+db=HikeDB("hikes_db.db")
+print("db", db)
+
+@app.route("/hikes/<int:hike_id>", methods=["OPTIONS"])
+def handle_cors_options(hike_id):
+    return "", 204, {
+        "Access-Control-Allow-Origin": "*", #when we get into authentication we won't be able to use the star
+        "Access-Control-Allow-Methods": "PUT, DELETE",
+        "Access-Control-Allow-Headers": "Content-Type"
+    }
+
+@app.route("/hikes", methods=["GET"])
+def retrieve_reviews():
+    # Allow CORS
+    db=HikeDB("hikes_db.db")
+    print("db", db)
+    reviews = db.getHikes()
+    return reviews, {"Access-Control-Allow-Origin" : "*"}
+
+@app.route("/hikes", methods=["POST"])
+def create_review():
+    print("The request data is: ", request.form)
+    name = request.form["name"]
+    location = request.form["location"]
+    miles = request.form["miles"]
+    rating = request.form["rating"]
+    review = request.form["review"]
+    picture = request.form["picture"]
+    db = HikeDB("hikes_db.db")
+    db.createHike(name, location, miles, rating, review, picture)
+    return "Created", 201, {"Access-Control-Allow-Origin" : "*"}
+
+@app.route("/hikes/<int:hike_id>", methods=["PUT"])
+def update_hike(hike_id):
+    print("update hike with ID", hike_id)
+    db = HikeDB("hikes_db.db")
+    hike = db.getHike(hike_id)
+    if hike:
+        name = request.form["name"]
+        location = request.form["location"]
+        miles = request.form["miles"]
+        rating = request.form["rating"]
+        review = request.form["review"]
+        picture = request.form["picture"]
+        db.updateHike(hike_id, name, location, miles, rating, review, picture)
+        return "Updated", 200, {"Access-Control-Allow-Origin" : "*"}
+    else:
+        return f"Hike with {hike_id} not found", 404, {"Access-Control-Allow-Origin" : "*"}
+
+@app.route("/hikes/<int:hike_id>", methods=["DELETE"])
+def delete_hike(hike_id):
+    print("deleting hike with ID", hike_id)
+    db = HikeDB("hikes_db.db")
+    hike = db.getHike(hike_id)
+    if hike:
+        db.deleteHike(hike_id)
+        return "Deleted", 200, {"Access-Control-Allow-Origin" : "*"}
+    else:
+        return f"Hike with {hike_id} not found", 404, {"Access-Control-Allow-Origin" : "*"}
+
+@app.route("/hikes/<int:hike_id>", methods=["GET"])
+def retreive_hike(hike_id):
+    db = HikeDB("hikes_db.db")
+    hike = db.getHike(hike_id)
+    if hike:
+        return hike, 200, {"Access-Control-Allow-Origin" : "*"}
+    else:
+        return f"Hike with id {hike_id} not found", 404, {"Access-Control-Allow-Origin" : "*"}
+
+def run():
+    app.run(port=8080)
+if __name__ == "__main__":
+    run()
